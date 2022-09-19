a4a6c424d4bd5db5728ea3cb2273644d50d678be
Raymond Ha <Haro@mail.uc.edu>
2022-09-18 21:29:12 -0400

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int creditCardBalance;
+        double interestRate;
+        double newMonthInterest;
+        double secondMonthInterest;
+
+        creditCardBalance = 5000;
+        interestRate = 0.17;
+        newMonthInterest = creditCardBalance * (1 + interestRate);
+        System.out.println("The balance for first month is " + newMonthInterest);
+
+        secondMonthInterest = newMonthInterest * (1 + interestRate);
+        System.out.println("The balance for second month is " + secondMonthInterest);
     }
 }
